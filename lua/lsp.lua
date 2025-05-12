29f2b27370e7243672cb8ae5bd855f91ed468349
Juroszek, Jan <jan.juroszek@volvocars.com>
2025-05-12 10:27:23 +0200

    add just some commets

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,5 +1,11 @@
--- vim.lsp.enable({"clangd"}) 
+-- i will create the system to have some machine specific file in some git
+-- ignored file that will specfy the which lsp server should be enabled
+-- 
+-- also formattes is problemstic, mayby i should familiarize with null-ls or
+-- check how lspconfig did it
+
 vim.lsp.enable({"lua_ls"})
+-- vim.lsp.enable({"clangd"}) 
 vim.lsp.enable({"pylsp"})
 vim.lsp.enable({"rust"})
 
