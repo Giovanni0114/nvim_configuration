b29f8766fbb21b6bae790c98ed70455843d05678
Juroszek, Jan <jan.juroszek@volvocars.com>
2025-05-12 10:25:06 +0200

    add python lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,4 +1,7 @@
-vim.lsp.enable({"clangd", "lua_ls", "rust"})
+-- vim.lsp.enable({"clangd"}) 
+vim.lsp.enable({"lua_ls"})
+vim.lsp.enable({"pylsp"})
+vim.lsp.enable({"rust"})
 
 vim.diagnostic.config({ virtual_text = true })
 
