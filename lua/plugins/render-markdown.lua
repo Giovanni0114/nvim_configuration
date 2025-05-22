881ba9fb1d7837c2d20ce80e8eabffd6b48589ad
Juroszek, Jan <jan.juroszek@volvocars.com>
2025-05-22 11:35:35 +0200

    markdown bullet list improved

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -2,5 +2,9 @@ return {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you use the mini.nvim suite
     lazy = true,
-    opts = {},
+    opts = {
+        bullet = {
+         icons =   {'•', '◦', '◆', '◇' }
+        }
+    },
 }
