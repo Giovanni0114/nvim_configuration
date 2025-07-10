fec29fdaf8fe64efc2b7419599ace0fd0ba28375
Juroszek, Jan <jan.juroszek@volvocars.com>
2025-07-10 10:25:37 +0200

    add pretty hover

--- lua/autocommands/lsp-attach.lua
+++ lua/autocommands/lsp-attach.lua
@@ -5,11 +5,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
             vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
-        map('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
+        map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
         map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
         map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
         map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+        map('K', require("pretty_hover").hover,  'pretty hover' )
 
         map('<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR><cmd>silent! wa<CR>', "[R]e[N]ame")
         map('<leader>ca', '<cmd>lua vim.lsp.buf.code_action({ apply = true })<CR>', '[C]ode [A]ction')
