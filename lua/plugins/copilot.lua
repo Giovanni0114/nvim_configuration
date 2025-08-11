9bccf11dc1dc52b4b3836f085f6964041c17613b
Juroszek, Jan <jan.juroszek@volvocars.com>
2025-08-11 12:55:11 +0200

    merge two AI plugins

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,22 @@
+return  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+          suggestion = { 
+                enabled = true,
+                auto_trigger = true,
+                debounce = 200,
+                trigger_on_accept = true,
+                keymap = {
+                  accept = "<A-l>",
+                  accept_word = false,
+                  accept_line = false,
+                  dismiss = "<A-[>",
+                },
+            },
+          copilot_model = "gpt-4o-copilot"
+        })
+      end,
+}
