54fd3366af6587723fb107e0eac88d8d94042661
Jan Juroszek <janko.juroszek@gmail.com>
2025-09-06 06:57:37 +0200

    experimental: disable lsp-based semantic tokes

--- lua/lsp-init.lua
+++ lua/lsp-init.lua
@@ -1,8 +1,16 @@
 -- i will create the system to have some machine specific file in some git
 -- ignored file that will specfy the which lsp server should be enabled
---
--- also formatting is problematic, maybe i should familiarize with null-ls or
--- check how lspconfig did it
+
+local function disable_highlight(lsp)
+    vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            if client and client.name == "clangd" then
+                client.server_capabilities.semanticTokensProvider = nil
+            end
+        end,
+    })
+end
 
 vim.lsp.enable({ "lua-ls" })
 vim.lsp.enable({ "clangd" })
@@ -12,3 +20,16 @@ vim.lsp.enable({ "html" })
 vim.lsp.enable({ "json" })
 
 vim.diagnostic.config({ virtual_text = true })
+
+-- disable_highlight("clangd") -- unused for now
+
+
+-- what if I want to disable all higlighs in all lsp servers?
+-- treesitter based highlighting should be enough
+--
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        client.server_capabilities.semanticTokensProvider = nil
+    end,
+})
