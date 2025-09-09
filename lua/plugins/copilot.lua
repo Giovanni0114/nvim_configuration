9516214e70e99ce482eda3fdf20e7f79559fc11f
Juroszek, Jan <jan.juroszek@volvocars.com>
2025-09-09 14:35:15 +0200

    SHUT UP COPILOT WHEN NOT ASKED

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,22 +1,25 @@
-return  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-          suggestion = { 
-                enabled = true,
-                auto_trigger = true,
-                debounce = 200,
-                trigger_on_accept = true,
-                keymap = {
-                  accept = "<A-l>",
-                  accept_word = false,
-                  accept_line = false,
-                  dismiss = "<A-[>",
-                },
+vim.keymap.set("n", "<leader>ac", function()
+    require("copilot").setup({
+        suggestion = {
+            enabled = true,
+            auto_trigger = true,
+            debounce = 200,
+            trigger_on_accept = true,
+            keymap = {
+                accept = "<A-l>",
+                accept_word = false,
+                accept_line = false,
+                dismiss = "<A-[>",
             },
-          copilot_model = "gpt-4o-copilot"
-        })
-      end,
+        },
+        copilot_model = "gpt-4o-copilot"
+    })
+end
+, { expr = true, silent = true })
+
+
+return {
+    "zbirenbaum/copilot.lua",
+    lazy = "VeryLazy",
+    cmd = "Copilot",
 }
