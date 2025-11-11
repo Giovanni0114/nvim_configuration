3981d1a80a7358710c0e4044b9ed833be180cf36
Jan Juroszek <janko.juroszek@gmail.com>
2025-11-11 23:46:50 +0100

    add text-objects for treesitter

---
+++ lua/plugins/treesitter/text-objects.lua
@@ -0,0 +1,37 @@
+return {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    -- lazy = "VeryLazy",
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            textobjects = {
+                lsp_interop = {
+                    enable = true,
+                    border = 'solid',
+                    floating_preview_opts = {},
+                    peek_definition_code = {
+                        ["<leader>df"] = "@function.outer",
+                    },
+                },
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                        ["ia"] = "@parameter.inner",
+                        ["aa"] = "@parameter.outer",
+                        ["il"] = "@loop.inner",
+                        ["al"] = "@loop.outer",
+                    },
+                    selection_modes = 'V';
+                    include_surrounding_whitespace = false,
+                },
+            },
+        })
+    end,
+}
