02a6283ed607f3eaadf7e219011bde3e11a71b19
Juroszek, Jan <jan.juroszek@volvocars.com>
2026-01-29 11:34:58 +0100

    add gitcommit snippets for Jira

---
+++ snippets/gitcommit.lua
@@ -0,0 +1,12 @@
+local ls = require("luasnip")
+
+return {
+    ls.snippet("jiracloses", {
+        ls.text_node("Jira: closes "),
+        ls.insert_node(1, "TICKET"),
+    }),
+    ls.snippet("jiracontrib", {
+        ls.text_node("Jira: contributes-to "),
+        ls.insert_node(1, "TICKET"),
+    }),
+}
