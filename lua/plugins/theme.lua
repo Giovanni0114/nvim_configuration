f407229b640d79832260b76e967315dd88941e5c
Juroszek, Jan <jan.juroszek@volvocars.com>
2026-06-09 09:09:20 +0200

    change theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,19 @@
+-- return {
+--     "catppuccin/nvim",
+--     name = "catppuccin",
+--     priority = 1000,
+--     config = function()
+--         vim.cmd.colorscheme 'catppuccin-mocha'
+--         vim.cmd.hi 'Comment gui=none'
+--     end,
+-- }
+
+
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "rose-pine/neovim",
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme 'catppuccin-mocha'
+        vim.cmd.colorscheme 'rose-pine'
         vim.cmd.hi 'Comment gui=none'
     end,
 }
