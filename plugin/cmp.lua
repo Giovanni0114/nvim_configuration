ffd1bdf9fdb7ec033545cb27819cf450bf3589d7
Jan Juroszek <janko.juroszek@gmail.com>
2026-06-13 17:40:57 +0200

    add priorities to the cmp plugin

--- plugin/cmp.lua
+++ plugin/cmp.lua
@@ -16,6 +16,23 @@ luasnip.config.setup {}
 
 require("luasnip.loaders.from_lua").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
 
+require("cmp_dictionary").setup({
+    paths = { "/usr/share/dict/words" },
+    first_case_insensitive = true,
+    exact_length = 4,
+})
+
+local function priority_comparator(entry1, entry2)
+    local diff = entry1.source:get_source_config().priority - entry2.source:get_source_config().priority
+    if diff > 0 then
+        return true
+    elseif diff < 0 then
+        return false
+    end
+    return nil
+end
+
+
 cmp.setup {
     snippet = {
         expand = function(args)
@@ -32,21 +49,20 @@ cmp.setup {
     },
 
     sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'path' },
-        { name = 'buffer' },
-        { name = "vim-dadbod-completion" },
-        { name = 'render-markdown' },
-        {
-            name = "dictionary",
-            keyword_length = 4,
+        { name = 'nvim_lsp',              priority = 10 },
+        { name = 'luasnip',               priority = 0 },
+        { name = 'path',                  priority = 0 },
+        { name = 'buffer',                priority = 0 },
+        { name = "vim-dadbod-completion", priority = 0 },
+        { name = 'render-markdown',       priority = 0 },
+        { name = "dictionary",            priority = -100, keyword_length = 4 },
+    },
+    sorting = {
+        comparators = {
+            priority_comparator,
+            cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
         },
     },
 }
-
-require("cmp_dictionary").setup({
-    paths = { "/usr/share/dict/words" },
-    first_case_insensitive = true,
-    exact_length = 4,
-})
