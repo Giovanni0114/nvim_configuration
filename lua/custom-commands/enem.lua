68cd4cac5841f348a65c6d3a6e24e4e0ab3d2c5c
Juroszek, Jan <jan.juroszek@volvocars.com>
2026-07-06 18:29:52 +0200

    sometimes you just commit minute too early

--- lua/custom-commands/enem.lua
+++ lua/custom-commands/enem.lua
@@ -1,4 +1,16 @@
-vim.api.nvim_create_user_command('Enem', function()
-  vim.cmd('enew')
+local function open_markdown(cmd)
+  vim.cmd(cmd)
   vim.bo.filetype = 'markdown'
+end
+
+vim.api.nvim_create_user_command('Enem', function()
+  open_markdown('enew')
+end, {})
+
+vim.api.nvim_create_user_command('Enemv', function()
+  open_markdown('vnew')
+end, {})
+
+vim.api.nvim_create_user_command('Enems', function()
+  open_markdown('new')
 end, {})
